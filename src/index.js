b7a3ce617b7e940a947e30c6a573d12b91962d92
André Santos <andrefs@andrefs.com>
2018-04-25 13:28:15 +0100

    Commented out unfinished methods

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Promise from 'bluebird';
 import { print, isBalanced } from './utils';
 
+// TODO update doc comments
+// TODO remove dead code
 
 // function createNode (parent, left, right, height, key, data) {
 //   return { parent, left, right, balanceFactor: height, key, data };
@@ -269,36 +271,37 @@ export default class AVLTree {
   }
 
 
-  /**
-   * Walk key range from `low` to `high`. Stops if `fn` returns a value.
-   * @param  {Key}      low
-   * @param  {Key}      high
-   * @param  {Function} fn
-   * @param  {*?}       ctx
-   * @return {SplayTree}
-   */
-  range(low, high, fn, ctx) {
-    const Q = [];
-    const compare = this._comparator;
-    let node = this._root, cmp;
+  // TODO
+  // /**
+  //  * Walk key range from `low` to `high`. Stops if `fn` returns a value.
+  //  * @param  {Key}      low
+  //  * @param  {Key}      high
+  //  * @param  {Function} fn
+  //  * @param  {*?}       ctx
+  //  * @return {SplayTree}
+  //  */
+  // range(low, high, fn, ctx) {
+  //   const Q = [];
+  //   const compare = this._comparator;
+  //   let node = this._root, cmp;
 
-    while (Q.length !== 0 || node) {
-      if (node) {
-        Q.push(node);
-        node = node.left;
-      } else {
-        node = Q.pop();
-        cmp = compare(node.key, high);
-        if (cmp > 0) {
-          break;
-        } else if (compare(node.key, low) >= 0) {
-          if (fn.call(ctx, node)) return this; // stop if smth is returned
-        }
-        node = node.right;
-      }
-    }
-    return this;
-  }
+  //   while (Q.length !== 0 || node) {
+  //     if (node) {
+  //       Q.push(node);
+  //       node = node.left;
+  //     } else {
+  //       node = Q.pop();
+  //       cmp = compare(node.key, high);
+  //       if (cmp > 0) {
+  //         break;
+  //       } else if (compare(node.key, low) >= 0) {
+  //         if (fn.call(ctx, node)) return this; // stop if smth is returned
+  //       }
+  //       node = node.right;
+  //     }
+  //   }
+  //   return this;
+  // }
 
 
   /**
@@ -435,19 +438,20 @@ export default class AVLTree {
   }
 
 
-  /**
-   * Removes and returns the node with smallest key
-   * @return {?Node}
-   */
-  pop () {
-    var node = this._root, returnValue = null;
-    if (node) {
-      while (node.left) node = node.left;
-      returnValue = { key: node.key, data: node.data };
-      this.remove(node.key);
-    }
-    return returnValue;
-  }
+  // TODO depends on .remove
+  // /**
+  //  * Removes and returns the node with smallest key
+  //  * @return {?Node}
+  //  */
+  // pop () {
+  //   var node = this._root, returnValue = null;
+  //   if (node) {
+  //     while (node.left) node = node.left;
+  //     returnValue = { key: node.key, data: node.data };
+  //     this.remove(node.key);
+  //   }
+  //   return returnValue;
+  // }
 
 
   /**
@@ -563,109 +567,110 @@ export default class AVLTree {
   }
 
 
-  /**
-   * Removes the node from the tree. If not found, returns null.
-   * @param  {Key} key
-   * @return {?Node}
-   */
-  remove (key) {
-    if (!this._root) return null;
+  // TODO
+  // /**
+  //  * Removes the node from the tree. If not found, returns null.
+  //  * @param  {Key} key
+  //  * @return {?Node}
+  //  */
+  // remove (key) {
+  //   if (!this._root) return null;
 
-    var node = this._root;
-    var compare = this._comparator;
-    var cmp = 0;
+  //   var node = this._root;
+  //   var compare = this._comparator;
+  //   var cmp = 0;
 
-    while (node) {
-      cmp = compare(key, node.key);
-      if      (cmp === 0) break;
-      else if (cmp < 0)   node = node.left;
-      else                node = node.right;
-    }
-    if (!node) return null;
+  //   while (node) {
+  //     cmp = compare(key, node.key);
+  //     if      (cmp === 0) break;
+  //     else if (cmp < 0)   node = node.left;
+  //     else                node = node.right;
+  //   }
+  //   if (!node) return null;
 
-    var returnValue = node.key;
-    var max, min;
+  //   var returnValue = node.key;
+  //   var max, min;
 
-    if (node.left) {
-      max = node.left;
+  //   if (node.left) {
+  //     max = node.left;
 
-      while (max.left || max.right) {
-        while (max.right) max = max.right;
+  //     while (max.left || max.right) {
+  //       while (max.right) max = max.right;
 
-        node.key = max.key;
-        node.data = max.data;
-        if (max.left) {
-          node = max;
-          max = max.left;
-        }
-      }
+  //       node.key = max.key;
+  //       node.data = max.data;
+  //       if (max.left) {
+  //         node = max;
+  //         max = max.left;
+  //       }
+  //     }
 
-      node.key  = max.key;
-      node.data = max.data;
-      node = max;
-    }
+  //     node.key  = max.key;
+  //     node.data = max.data;
+  //     node = max;
+  //   }
 
-    if (node.right) {
-      min = node.right;
+  //   if (node.right) {
+  //     min = node.right;
 
-      while (min.left || min.right) {
-        while (min.left) min = min.left;
+  //     while (min.left || min.right) {
+  //       while (min.left) min = min.left;
 
-        node.key  = min.key;
-        node.data = min.data;
-        if (min.right) {
-          node = min;
-          min = min.right;
-        }
-      }
+  //       node.key  = min.key;
+  //       node.data = min.data;
+  //       if (min.right) {
+  //         node = min;
+  //         min = min.right;
+  //       }
+  //     }
 
-      node.key  = min.key;
-      node.data = min.data;
-      node = min;
-    }
+  //     node.key  = min.key;
+  //     node.data = min.data;
+  //     node = min;
+  //   }
 
-    var parent = node.parent;
-    var pp     = node;
-    var newRoot;
+  //   var parent = node.parent;
+  //   var pp     = node;
+  //   var newRoot;
 
-    while (parent) {
-      if (parent.left === pp) parent.balanceFactor -= 1;
-      else                    parent.balanceFactor += 1;
+  //   while (parent) {
+  //     if (parent.left === pp) parent.balanceFactor -= 1;
+  //     else                    parent.balanceFactor += 1;
 
-      if        (parent.balanceFactor < -1) {
-        // inlined
-        //var newRoot = rightBalance(parent);
-        if (parent.right.balanceFactor === 1) rotateRight(parent.right);
-        newRoot = rotateLeft(parent);
+  //     if        (parent.balanceFactor < -1) {
+  //       // inlined
+  //       //var newRoot = rightBalance(parent);
+  //       if (parent.right.balanceFactor === 1) rotateRight(parent.right);
+  //       newRoot = rotateLeft(parent);
 
-        if (parent === this._root) this._root = newRoot;
-        parent = newRoot;
-      } else if (parent.balanceFactor > 1) {
-        // inlined
-        // var newRoot = leftBalance(parent);
-        if (parent.left.balanceFactor === -1) rotateLeft(parent.left);
-        newRoot = rotateRight(parent);
+  //       if (parent === this._root) this._root = newRoot;
+  //       parent = newRoot;
+  //     } else if (parent.balanceFactor > 1) {
+  //       // inlined
+  //       // var newRoot = leftBalance(parent);
+  //       if (parent.left.balanceFactor === -1) rotateLeft(parent.left);
+  //       newRoot = rotateRight(parent);
 
-        if (parent === this._root) this._root = newRoot;
-        parent = newRoot;
-      }
+  //       if (parent === this._root) this._root = newRoot;
+  //       parent = newRoot;
+  //     }
 
-      if (parent.balanceFactor === -1 || parent.balanceFactor === 1) break;
+  //     if (parent.balanceFactor === -1 || parent.balanceFactor === 1) break;
 
-      pp     = parent;
-      parent = parent.parent;
-    }
+  //     pp     = parent;
+  //     parent = parent.parent;
+  //   }
 
-    if (node.parent) {
-      if (node.parent.left === node) node.parent.left  = null;
-      else                           node.parent.right = null;
-    }
+  //   if (node.parent) {
+  //     if (node.parent.left === node) node.parent.left  = null;
+  //     else                           node.parent.right = null;
+  //   }
 
-    if (node === this._root) this._root = null;
+  //   if (node === this._root) this._root = null;
 
-    this._size--;
-    return returnValue;
-  }
+  //   this._size--;
+  //   return returnValue;
+  // }
 
 
   /**
